6f93014a6115bfc2cf545c4d0c43aa4517a82486
EGLIK <eglik@odin.vision>
2019-09-30 17:06:38 +0900

    test: array linear interpolation

---
+++ Assets/Interpolation.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interpolation
+{
+    public static Vector2 Interpolation(List<Vector2> list, float t)
+    {
+
+        float range = 1f / (list.Count - 1);
+        int index = Mathf.FloorToInt(t / range);
+        return Vector3.Lerp(list[index], list[index + 1], Mathf.InverseLerp(range * index, range * (index + 1), t));
+    }
+}
---
+++ Assets/Test.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[ExecuteInEditMode]
+public class Test : MonoBehaviour
+{
+    [SerializeField] private RawImage rawImage;
+    [SerializeField] private List<Vector2> horizontal;
+
+    private void Update()
+    {
+        Texture2D texture2D = new Texture2D(1920, 1080);
+
+        for (int i = 0; i < 1920; i++)
+        {
+            for (int k = 0; k < 1080; k++)
+            {
+                texture2D.SetPixel(i, k, Color.white);
+            }
+        }
+
+        for (int i = 0; i < 1920; i++)
+        {
+            var pos = Interpolation.Interpolation(horizontal, i / 1920f);
+            texture2D.SetPixel((int)pos.x, (int)pos.y, Color.black);
+            texture2D.SetPixel((int)pos.x, (int)pos.y + 1, Color.black);
+            texture2D.SetPixel((int)pos.x + 1, (int)pos.y, Color.black);
+            texture2D.SetPixel((int)pos.x + 1, (int)pos.y + 1, Color.black);
+            //Debug.Log(pos + $"{i / 1920f}");
+        }
+        texture2D.Apply();
+        rawImage.texture = texture2D;
+    }
+}
